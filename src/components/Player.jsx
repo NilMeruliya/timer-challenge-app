882ad86acd0adb7ee65f8f75c714fa99db92e4f6
Nil Meruliya <100191375+NilMeruliya@users.noreply.github.com>
2024-02-14 16:15:54 -0500

    fetching user name

---
+++ src/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useRef, useState } from "react";
+
+export default function Player() {
+  const userName = useRef(null);
+
+  const [displayPlayerName, setDisplayPlayerName] = useState("");
+
+  const handleSubmit = (e) => {
+    // e.preventDefault(); // use only while work with (form) tag
+    setDisplayPlayerName(userName.current.value);
+    console.log(userName.current.value);
+    userName.current.value = ''
+  };
+  return (
+    <section id="player">
+      <h2>
+        {displayPlayerName ? `Welcome ${displayPlayerName}` : `Welcome unknown entity`}
+        {/* Welcome {displayPlayerName ?? `Welcome unknown entity`} */}
+
+      </h2>
+
+      <p>
+        <input type="text" ref={userName} />
+        <button onClick={handleSubmit}>Set Name</button>
+      </p>
+    </section>
+  );
+}
